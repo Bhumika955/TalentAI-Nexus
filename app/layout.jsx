25b8f587b9c6e9facbea2c3436b30d51a14b81a7
Bhumika955 <bhumikabanke1024@gmail.com>
2026-03-30 01:34:45 +0530

    refactor: separate metadata and client layout to fix server/client component conflict

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import ClientLayout from "./components/ClientLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <ClientLayout>
+          {children}
+        </ClientLayout>
       </body>
     </html>
   );
